f1735014d8241dc83ee556ce513a70104ab4deaf
William Washington <william.washington@live.com>
2019-05-09 20:32:20 -0400

    Add files via upload

---
+++ DirUpButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirUpButton : MonoBehaviour
+{
+
+	private RectTransform rect;
+	private ParticleSystem part;
+	private Transform trans;
+	private Player1Controller button;
+	private bool isPressed;
+	private bool playing;
+
+	public GameObject xboxController;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		rect = GetComponent<RectTransform>();
+		part = GetComponent<ParticleSystem>();
+		trans = rect.transform;
+		button = xboxController.GetComponent<Player1Controller>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (button.dPadUp == true) //if A button goes down
+		{
+			if (part.isPlaying == false) //and if particle system is not playing
+			{
+				trans.localPosition = new Vector3(Random.Range(-70, -1000), Random.Range(-500, 500), Random.Range(200, 300)); //pick a random location within the button square and move to it
+				part.Play(true); //play the animation
+				Component[] children = GetComponentsInChildren<ParticleSystem>(); //create an array of all the children of the particle system
+				foreach (ParticleSystem childParticleSystem in children) //for each child
+				{
+					childParticleSystem.Play(true); //play the partcle system
+				}
+			}
+		}
+	}
+}
---
+++ Player1Controller.cs
@@ -0,0 +1,297 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Controller : MonoBehaviour {
+
+	//buttons
+	public bool aButton;
+	public bool bButton;
+	public bool xButton;
+	public bool yButton;
+	public bool lbButton;
+	public bool rbButton;
+	public bool selectButton;
+	public bool startButton;
+	public bool leftStickButton;
+	public bool rightStickButton;
+	
+	//axes
+	public bool leftTrigger;
+	public bool rightTrigger;
+	public bool leftStickRight;
+	public bool leftStickLeft;
+	public bool leftStickDown;
+	public bool leftStickUp;
+	public bool rightStickRight;
+	public bool rightStickLeft;
+	public bool rightStickDown;
+	public bool rightStickUp;
+	public bool dPadRight;
+	public bool dPadLeft;
+	public bool dPadUp;
+	public bool dPadDown;
+	
+	// Update is called once per frame
+	void Update () {
+
+		#region Xbox Controller Buttons
+
+		//--> Core Buttons Start
+		if (Input.GetButtonDown("XboxA"))
+		{			
+			//A Button
+			print("A button");
+			aButton = true;
+		} else
+		{
+			aButton = false;
+		}
+		if (Input.GetButton("XboxB"))
+		{
+			//B Button
+			print("B button");
+			bButton = true;
+		}
+		else
+		{
+			bButton = false;
+		}
+		if (Input.GetButton("XboxX"))
+		{
+			//X Button
+			print("X button");
+			xButton = true;
+		}
+		else
+		{
+			xButton = false;
+		}
+		if (Input.GetButton("XboxY"))
+		{
+			//Y Button
+			print("Y button");
+			yButton = true;
+		}
+		else
+		{
+			yButton = false;
+		}
+
+		if (Input.GetButton("XboxLB"))
+		{
+			//Left Bumper Button
+			print("LB button");
+			lbButton = true;
+		}
+		else
+		{
+			lbButton = false;
+		}
+		if (Input.GetButton("XboxRB"))
+		{
+			//Right Bumper Button
+			print("RB button");
+			rbButton = true;
+		}
+		else
+		{
+			rbButton = false;
+		}
+		//--> Core Buttons End
+
+		//--> Start/Select Buttons Start
+		if (Input.GetButton("XboxSelect"))
+		{
+			//Select Button
+			print("Select button");
+			selectButton = true;
+		}
+		else
+		{
+			selectButton = false;
+		}
+		if (Input.GetButton("XboxStart"))
+		{
+			//Start Button
+			print("Start button");
+			startButton = true;
+		}
+		else
+		{
+			startButton = false;
+		}
+		//--> Start/Select Buttons End
+
+		//--> Xbox Stick Clicks Start
+		if (Input.GetButton("XboxClickLeftStick"))
+		{
+			//Left Stick Click Button
+			print("Left Stick button");
+			leftStickButton = true;
+		}
+		else
+		{
+			leftStickButton = false;
+		}
+		if (Input.GetButton("XboxClickRightStick"))
+		{
+			//Right Stick Click Button
+			print("Right Stick button");
+			rightStickButton = true;
+		}
+		else
+		{
+			rightStickButton = false;
+		}
+		//--> Xbox Stick Clicks End
+
+		#endregion
+
+		#region Xbox Controller Axes
+
+		if (Input.GetAxis("XboxLeftTrigger") > 0) {
+			//Left Trigger Axis
+			print("Left Trigger");
+			leftTrigger = true;
+		}
+		else
+		{
+			leftTrigger = false;
+		}
+		if (Input.GetAxis("XboxRightTrigger") > 0)
+		{
+			//Right Trigger Axis
+			print("Right Trigger");
+			rightTrigger = true;
+		}
+		else
+		{
+			rightTrigger = false;
+		}
+		if (Input.GetAxis("LeftStickXAxis") > 0)
+		{
+			//Left Stick X Axis RIGHT
+			print("LeftStickXRight");
+			leftStickRight = true;
+		}
+		else
+		{
+			leftStickRight = false;
+		}
+		if (Input.GetAxis("LeftStickXAxis") < 0)
+		{
+			//Left Stick X Axis LEFT
+			print("LeftStickXLeft");
+			leftStickLeft = true;
+		}
+		else
+		{
+			leftStickLeft = false;
+		}
+		if (Input.GetAxis("LeftStickYAxis") > 0)
+		{			
+			//Left Stick Y Axis DOWN
+			print("LeftStickYDown");
+			leftStickDown = true;
+		}
+		else
+		{
+			leftStickDown = false;
+		}
+		if (Input.GetAxis("LeftStickYAxis") < 0)
+		{			
+			//Left Stick Y Axis UP
+			print("LeftStickYUp");
+			leftStickUp = true;
+		}
+		else
+		{
+			leftStickUp = false;
+		}
+		if (Input.GetAxis("RightStickXAxis") > 0)
+		{			
+			//Right Stick X Axis RIGHT
+			print("RightStickXRight");
+			rightStickRight = true;
+		}
+		else
+		{
+			rightStickRight = false;
+		}
+		if (Input.GetAxis("RightStickXAxis") < 0)
+		{			
+			//Right Stick X Axis LEFT
+			print("RightStickXLeft");
+			rightStickLeft = true;
+		}
+		else
+		{
+			rightStickLeft = false;
+		}
+		if (Input.GetAxis("RightStickYAxis") > 0)
+		{			
+			//Right Stick Y Axis DOWN
+			print("RightStickYDown");
+			rightStickDown = true;
+		}
+		else
+		{
+			rightStickDown = false;
+		}
+		if (Input.GetAxis("RightStickYAxis") < 0)
+		{			
+			//Right Stick Y Axis UP
+			print("RightStickYUp");
+			rightStickUp = true;
+		}
+		else
+		{
+			rightStickUp = false;
+		}
+		if (Input.GetAxis("DPadXAxis") > 0)
+		{			
+			//DPad X Axis RIGHT
+			print("DPad X Right");
+			dPadRight = true;
+		}
+		else
+		{
+			dPadRight = false;
+		}
+		if (Input.GetAxis("DPadXAxis") < 0)
+		{			
+			//DPad X Axis LEFT
+			print("DPad X Left");
+			dPadLeft = true;
+		}
+		else
+		{
+			dPadLeft = false;
+		}
+		if (Input.GetAxis("DPadYAxis") > 0)
+		{			
+			//DPad Y Axis UP
+			print("DPad Y Up");
+			dPadUp = true;
+		}
+		else
+		{
+			dPadUp = false;
+		}
+		if (Input.GetAxis("DPadYAxis") < 0)
+		{			
+			//DPad Y Axis DOWN
+			print("DPad Y Down");
+			dPadDown = true;
+		}
+		else
+		{
+			dPadDown = false;
+		}
+
+		#endregion
+
+	}
+}
